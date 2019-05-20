d836e8434b374e7983fd8c1fa38fa6c45e03cfd0
unknown <san91130324@gmail.com>
2019-05-20 10:28:43 +0300

    added help

---
+++ src/main/java/by/zhuk/bdam/starter/ParametersParser.java
@@ -0,0 +1,11 @@
+package by.zhuk.bdam.starter;
+
+import java.util.Map;
+
+public class ParametersParser {
+
+    public Map<String,String> parseParameters(String[] args){
+        return null;
+    }
+
+}
--- src/main/java/by/zhuk/bdam/starter/Starter.java
+++ src/main/java/by/zhuk/bdam/starter/Starter.java
@@ -5,10 +5,7 @@ import by.zhuk.bdam.domain.core.AppType;
 import by.zhuk.bdam.domain.core.JobConfig;
 import by.zhuk.bdam.domain.core.JobProblemSolution;
 import by.zhuk.bdam.domain.core.Report;
-import by.zhuk.bdam.exception.JobDumpException;
-import by.zhuk.bdam.exception.ParseConfigException;
-import by.zhuk.bdam.exception.ReportSendException;
-import by.zhuk.bdam.exception.WriteFileException;
+import by.zhuk.bdam.exception.*;
 import by.zhuk.bdam.executor.JobExecutor;
 import by.zhuk.bdam.genirator.JsonJobProblemReportGenerator;
 import by.zhuk.bdam.infodumper.AppInfoJsonDumper;
@@ -25,21 +22,42 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Map;
 
 public class Starter {
     private static final Logger LOGGER = LoggerFactory.getLogger(Starter.class);
+    private static final String HELP_FILE = "/help.txt";
+    private static final String HELP_PARAMETER = "--help";
 
+    private static void showHelpMessage() {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(Starter.class.getResourceAsStream(HELP_FILE)))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            LOGGER.error("Error to view help message " + e);
+        }
+    }
 
     public static void main(String[] args) {
+        for (String arg : args) {
+            if (arg.equals(HELP_PARAMETER)) {
+                showHelpMessage();
+                return;
+            }
+        }
         if (args.length < 4) {
-            LOGGER.error("Invalid args count: must set up mode, config file and new config file name, sender report type and param for sender");
+            LOGGER.error("Invalid args count: must set up app type, config file and new config file name, sender report type and param for sender");
             return;
         }
-        String stringAppType = args[0];
-        String configFile = args[1];
-        String newConfigFile = args[2];
-        String senderType = args[3];
+        String stringAppType = args[1];
+        String configFile = args[3];
+        String newConfigFile = args[5];
+        String senderType = args[7];
         if (AppType.contains(stringAppType)) {
             LOGGER.error("Not found application type");
             return;
@@ -62,8 +80,8 @@ public class Starter {
         ReportSender sender = appType.getReportSenderFactory().createReportSenderByAppArgs(ReportSenderFactory.SenderType.valueOf(senderType.toUpperCase()),args);
         try {
             JobConfig config = parser.parse(configFile);
-//            String appId = executor.executeJob(config);
-            JSONObject metric = dumper.dump("local-1551800625858", config);
+            String appId = executor.executeJob(config);
+            JSONObject metric = dumper.dump(appId, config);
             JSONObject analysis = analyst.analyze(metric);
             Map<String, JobProblemSolution> solutionMap = problemSolver.solveProblems(analysis, config);
 
@@ -77,6 +95,8 @@ public class Starter {
             sender.sendReport(report);
         } catch (ParseConfigException | JobDumpException | WriteFileException | ReportSendException e) {
             LOGGER.error("Error", e);
+        } catch (JobExecuteException e) {
+            e.printStackTrace();
         }
     }
 
